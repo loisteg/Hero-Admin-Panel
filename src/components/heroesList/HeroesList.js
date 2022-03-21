e6f7d3430476baba81f0673349782e92d75af6c1
Loisteg <loisteg@outlook.com>
2022-03-21 20:14:58 +0200

    Added CSStransitions

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,6 +1,7 @@
 import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -38,19 +39,24 @@ const HeroesList = () => {
 
     const renderHeroesList = (arr) => {
         if (arr.length === 0) {
-            return <h5 className="text-center mt-5">Героев пока нет</h5>
+            return <CSSTransition  timeout={0} classNames="hero">
+                    <h5 className="text-center mt-5">Героев пока нет</h5>
+                </CSSTransition>
+            
         }
 
         return arr.map(({id, ...props}) => {
-            return <HeroesListItem {...props} onDelete={() => onDelete(id)}/>
+            return <CSSTransition key={id} timeout={1000} classNames="hero">
+                        <HeroesListItem {...props} onDelete={() => onDelete(id)}/>
+                    </CSSTransition>
         })
     }
 
     const elements = renderHeroesList(filteredHeroes);
     return (
-        <ul>
+        <TransitionGroup component="ul">
             {elements}
-        </ul>
+        </TransitionGroup>
     )
 }
 
