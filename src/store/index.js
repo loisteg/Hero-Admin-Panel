9f4dcb4c1706aa993a52181ac06a9228188be1a8
Loisteg <loisteg@outlook.com>
2022-03-22 13:25:20 +0200

    Added configure store from toolkits, deleted superfluous

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -13,26 +12,10 @@ const stringMiddleWare = () => (next) => (action) => {
     return next(action)
 }
 
-
-// const enhancer = (createStore) => (...args) => {
-//     const store = createStore(...args);
-
-//     const oldDispatch = store.dispatch;
-//     store.dispatch = (action) => {
-//         if (typeof action === 'string') {
-//             return oldDispatch({
-//                 type: action
-//             })
-//         }
-//         return oldDispatch(action)
-//     }
-//     return store;
-// }
-
-const store = createStore(
-                            combineReducers({heroes, filters}), 
-                            compose(applyMiddleware(ReduxThunk, stringMiddleWare), 
-                                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-                            );
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: getDefaultMiddleWare => getDefaultMiddleWare().concat(stringMiddleWare) ,
+    devTools: process.env.NODE_ENV !== 'production'
+})
 
 export default store;
